01aa1c6cccb4d3145f4799a0027849404f302f7c
coderrabbie <golamrabbi.dv@gmail.com>
2022-10-11 12:27:08 +0600

    quiz section added

--- src/components/TopicDetails/TopicDetails.js
+++ src/components/TopicDetails/TopicDetails.js
@@ -9,18 +9,32 @@ const TopicDetails = () => {
   console.log(topicData);
   console.log(topicOptions);
   return (
-    <div className="px-20 flex flex-col gap-5 justify-center items-center py-10">
+    <div className="sm:px-20 px-5 flex flex-col gap-5 justify-center items-center py-10">
       <h1 className="text-3xl">
         Topic Name: <span className="text-pink-500">{name}</span>
       </h1>
       {topicData.map((topic) => (
-        <div key={topic.id} className="bg-slate-500 w-[700px] p-5">
+        <div
+          key={topic.id}
+          className="bg-pink-400 rounded-xl shadow-lg lg:w-3/5 w-full p-5"
+        >
           <h2> Q:{topic.question}</h2>
-          <div className="grid grid-cols-2">
+          <table className="grid lg:grid-cols-2 grid-cols-1">
             {topic.options.map((option) => (
-              <input type="radio" />
+              <div className="flex gap-3 text-white items-center">
+                <label htmlFor={option} className="flex gap-3 mb-3">
+                  <input
+                    className=""
+                    type="radio"
+                    name="option"
+                    id={option}
+                    value={option}
+                  />{" "}
+                  {option}
+                </label>
+              </div>
             ))}
-          </div>
+          </table>
         </div>
       ))}
     </div>
