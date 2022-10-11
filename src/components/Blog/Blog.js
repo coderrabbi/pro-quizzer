39e299e660a25a82d5a67765bb17ddff84153101
coderrabbie <golamrabbi.dv@gmail.com>
2022-10-11 21:02:11 +0600

    blog qna added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,7 +1,17 @@
 import React from "react";
+import data from "../../qus";
 
 const Blog = () => {
-  return <div>this is blog components</div>;
+  return (
+    <div className="lg:px-20 py-10 px-10">
+      {data.map((qna) => (
+        <div key={qna.id} className="flex flex-col gap-5">
+          <h1 className="text-3xl text-slate-500 ">Q:{qna.Q}</h1>
+          <p className="text-[18px]">Ans:{qna.ans}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Blog;
--- src/qus.js
+++ src/qus.js
@@ -1,13 +1,19 @@
 const qna = [
   {
+    img: "https://res.cloudinary.com/practicaldev/image/fetch/s--vtI2NHvd--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/q0lj87mz6whntv2zbxdm.png",
+    id: 1,
     Q: "what is the purpose of react router?",
     ans: "React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.Let us create a simple application to React to understand how the React Router works. The application will contain three components: home component, about a component, and contact component. We will use React Router to navigate between these components.",
   },
   {
+    id: 2,
+    img: "https://bs-uploads.toptal.io/blackfish-uploads/components/seo/content/og_image_file/og_image/777655/react-context-api-4929b3703a1a7082d99b53eb1bbfc31f.png",
     Q: "How does context api works?",
     ans: "The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to 'prop drilling' or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.",
   },
   {
+    id: 3,
+    img: "https://tsh.io/wp-content/uploads/2020/10/react-hooks-best-practices-lead_.jpg",
     Q: "what is useRef?",
     ans: "Refs are a function provided by React to access the DOM element and the React element that you might have created on your own. They are used in cases where we want to change the value of a child component, without making use of props and all. They also provide us with good functionality as we can use callbacks with them.",
   },
